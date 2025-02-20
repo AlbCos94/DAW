40f0abc5a1985e590f624abe6603bcc9588d5a08
Albert Costa <albertcr1994@gmail.com>
2025-02-20 23:10:28 +0100

    First commit Campus Project

---
+++ CampusJava/EAC07/src/universitat/AulaInformatica.java
@@ -0,0 +1,97 @@
+/*
+ * Classe que defineix una aula d'informàtica d'una universitat. 
+ * Es defineix pel seu codi, número d'aula, el seu cost per dia i el tamany de l'aula en metres quadrats.
+ */
+package universitat;
+
+
+/**
+ *
+ * @author fgarin
+ */
+public class AulaInformatica {
+    private final static Scanner DADES = new Scanner(System.in);
+
+    private String codi;
+    private int numeroAula;
+    private double cosPerDia;
+    private double areaEnMetresQuadrats;
+
+    /*
+     * TODO CONSTRUCTOR
+     *
+     * Nom del mètode: AulaInformatica          
+     *
+     * Paràmetres: valors per tots els atributs de la classe.
+     *
+     * Accions:
+     * - Assignar als atributs corresponents els valors passats com a paràmetres.
+     */
+    
+
+    /*
+     * TODO Heu d'implementar tots els mètodes accessors possibles.  
+     */
+
+    
+
+    /*
+     * TODO
+     *
+     * Nom del mètode: addAulaInformatica
+     *
+     * Paràmetres: cap
+    
+     * Accions:
+     * - Demanar a l'usuari les dades per consola per crear una nova aula de informatica. Les dades a
+     *   demanar són les que necessita el constructor.
+     *
+     * Retorn: Objecte AulaInformatica creat.
+     */
+    
+
+    /*
+     * TODO
+     *
+     * Nom del mètode: updateAulaInformatica
+     *
+     * Paràmetres: cap
+     *
+     * Accions:
+     * - Demanar a l'usuari que introdueixi les noves dades de l'aula de informatica i modificar els
+     *   atributs corresponents d'aquesta aula.
+     * - Li heu de mostrar a l'usuari els valors dels atributs abans de modificar-los.
+     *
+     * Retorn: cap
+     */
+    
+
+    /*
+     * TODO
+     *
+     * Nom del mètode: showAulaInformatica
+     * 
+     * Accions:
+     * - Mètode per imprimir les dades de l'aula de informatica i el seu cost de manteniment.
+     *
+     * Retorn: cap
+     */
+    
+
+    /*
+     * TODO
+     *
+     * Nom del mètode: costManteniment
+     *
+     * Paràmetres: cap
+     *
+     * Accions:
+     * - Mètode que retornarà el cost de manteniment de l'aula actual. 
+     * - El cost de manteniment per dia és un 30% del cost per dia de l'aula + un 5% de la seva àrea
+     *
+     * Retorn: cost de manteniment de l'aula (double).
+     */
+    
+
+
+}
---
+++ CampusJava/EAC07/src/universitat/Laboratori.java
@@ -0,0 +1,97 @@
+/*
+ * Classe que defineix un Laboratori d'una universitat.
+ * Es defineix pel seu codi, numeroLaboratori, cost per dia i la seva capacitat en nombre de persones.
+ */
+package universitat;
+
+
+/**
+ *
+ * @author fgarin
+ */
+public class Laboratori {
+
+    private String codi;
+    private int numeroLaboratori;
+    private double costPerDia;
+    private int capacitat;
+
+    /*
+     * TODO CONSTRUCTOR
+     *
+     * Nom del mètode: Laboratori
+     *
+     * Paràmetres: valors per tots els atributs de la classe.
+     *
+     * Accions:
+     * - Assignar als atributs corresponents els valors passats com a paràmetres.
+     */
+   
+    
+    /*
+     * TODO Heu d'implementar tots els mètodes accessors possibles.  
+     */
+
+
+    /*
+     * TODO
+     *
+     * Nom del mètode: addLaboratori
+     *
+     * Paràmetres: cap
+     *
+     * Accions:
+     * - Demanar les dades per consola per crear un Laboratori. 
+     *   Les dades a demanar són les que necessita
+     *   el constructor.
+     * 
+     * Retorn: Objecte Laboratori creat.
+     */
+
+
+    /*
+     * TODO
+     *
+     * Nom del mètode: updateLaboratori
+     *
+     * Paràmetres: cap
+     *
+     * Accions:
+     * - Demanar a l'usuari que introdueixi les noves dades del Laboratori i modificar els
+     *   atributs corresponents d'aquest Laboratori.
+     * - Li heu de mostrar a l'usuari els valors dels atributs abans de modificar-los.
+     *
+     * Retorn: cap
+     */
+
+
+    /*
+     * TODO
+     *
+     * Nom del mètode: showLaboratori
+     *
+     * Paràmetres: cap
+     *
+     * Accions:
+     * - Mètode per mostrar les dades del Laboratori actual i el cost del seu manteniment.
+     * 
+     * Retorn: cap    
+     */
+  
+
+    /*
+     * TODO
+     *
+     * Nom del mètode: costManteniment
+     *
+     * Paràmetres: cap
+     *
+     * Accions:
+     * - Mètode que retornarà el cost del manteniment del Laboratori actual.
+     * - El cost del manteniment per dia és un 35% del cost per dia del laboratori + un 10% de la seva capacitat
+     *
+     * Retorn: Cost de manteniment del laboratori actual (double). 
+     */
+ 
+
+}
