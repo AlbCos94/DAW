e60aa74af1c4fa01039614261751963f807fc8bd
Albert Costa <79721669+AlbCos94@users.noreply.github.com>
2025-03-27 11:38:49 +0100

    Add files via upload

--- CampusJava/EAC09/src/universitat/GestorUniversitatsException.java
+++ CampusJava/EAC09/src/universitat/GestorUniversitatsException.java
@@ -7,9 +7,22 @@ package universitat;
  */
 public class GestorUniversitatsException extends Exception {
     
+    private String codi; // codi de causa de l'exepció
+
+    public GestorUniversitatsException (String codi){
+
+        this.codi = codi;
+    }
+
+
     @Override
     public String getMessage() {
         
+
+        return "Codi causa: " + this.codi + " - Missatge: " + super.getMessage();
+
+
+        /* 
         String stringCause = new String();
         String textMessage = new String();
 
@@ -22,8 +35,8 @@ public class GestorUniversitatsException extends Exception {
         
         }
         
-        return stringCause + " - " +textMessage;
-        
+        return stringCause + " - " + textMessage;
+        */
     }
 
     
